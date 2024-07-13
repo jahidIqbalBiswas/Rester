06cf6f1c0567b679572b42ea38c61c617a41b802
jahidIqbalBiswas <official.jahidbiswas@gmail.com>
2024-07-13 18:33:50 +0600

    completed

--- js/app.js
+++ js/app.js
@@ -13,3 +13,49 @@ desktopRightSidebarOpener.addEventListener("click",() => {
 rightSidebarCloseBtn.addEventListener("click",() => {
     desktopRightSidebar.classList.remove('open-right-sidebar')
 })
+
+//swiper slider codes
+// heroSlider
+{        const heroSlider = new Swiper('#heroSlider', {
+// Navigation arrows
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+}
+});
+// heroSlider end
+// heroSliderThumbs
+const heroSliderThumbs = new Swiper ('#heroSliderThumbs', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: false,
+    slideToClickedSlide: true,
+});
+heroSlider.controller.control = heroSliderThumbs;
+heroSliderThumbs.controller.control = heroSlider;}
+// heroSliderThumbs end
+// foodItemsSlider
+const foodItemsSlider = new Swiper("#foodItemsSlider",{
+    slidesPerView: 2,
+    spaceBetween:30,
+    loop:true,
+    grabCursor:true,
+    navigation: {
+    nextEl: '.food-slider-prev',
+    prevEl: '.food-slider-next',
+},
+    breakpoints:{
+    768:{
+    slidesPerView:3
+    },
+    992:{
+    slidesPerView:4
+    },
+    1200:{
+    slidesPerView:5
+    },
+    1400:{
+    slidesPerView:6
+    },
+    }
+})
